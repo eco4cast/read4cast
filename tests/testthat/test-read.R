19c5e90b3228ca2a5253d683429129864a84d619
Carl <cboettig@gmail.com>
2022-03-23 22:04:36 +0000

    unit tests

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,5 +1,18 @@
 
-test_that("reading", {
+test_that("ncdf by URL", {
   
+  fc <- 
+    paste0("https://data.ecoforecast.org/forecasts/",
+           "terrestrial_30min/terrestrial_30min-2022-01-01-hist30min.nc")
+  df <- read_forecast(fc)
+  expect_s3_class(df, "tbl_df")
+  
+})
+test_that("csv.gz by URL", {
+    
+  fc <- paste0("https://data.ecoforecast.org/forecasts/aquatics/",
+               "aquatics-2020-09-01-EFInull.csv.gz")
+  df <- read_forecast(fc)
+  expect_s3_class(df, "tbl_df")
   
 })
