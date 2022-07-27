28829bf8de3648eaa9b99ba44e4e853347880275
Carl Boettiger <cobettig@gmail.com>
2022-07-27 23:22:09 +0000

    tests

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -10,9 +10,33 @@ test_that("ncdf by URL", {
 })
 test_that("csv.gz by URL", {
     
-  fc <- paste0("https://data.ecoforecast.org/forecasts/aquatics/",
+  fc <- paste0("https://data.ecoforecast.org/neon4cast-forecasts/aquatics/",
                "aquatics-2020-09-01-EFInull.csv.gz")
   df <- read_forecast(fc)
+  expect_s3_class(df, "tbl_df")
+  
+})
+
+test_that("csv.gz by S3", {
+  
+  s3 <- arrow::s3_bucket("neon4cast-forecasts/aquatics", 
+                  endpoint_override = "data.ecoforecast.org", 
+                  anonymous = TRUE)
+  key <- "aquatics-2020-09-01-EFInull.csv.gz"
+
+  df <- read_forecast(key, s3=s3)
+  expect_s3_class(df, "tbl_df")
+  
+})
+
+test_that("nc by S3", {
+  ## ncdf versions are converted to csv due to pivoting
+  s3 <- arrow::s3_bucket("forecasts", 
+                         endpoint_override = "data.ecoforecast.org", 
+                         anonymous = TRUE)
+  key <- "terrestrial_30min/terrestrial_30min-2022-01-01-hist30min.nc"
+  
+  df <- read_forecast(key, s3=s3)
   expect_s3_class(df, "tbl_df")
   
 })
